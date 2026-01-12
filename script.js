185e932e32807b6dcdaaa8eab4c5c11ba0852d72
praveendolai93-star <praveendolai93@gmail.com>
2026-01-12 18:32:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function calculateEMI() {
+    let P = document.getElementById("loanAmount").value;
+    let R = document.getElementById("interestRate").value;
+    let N = document.getElementById("loanTenure").value;
+
+    if (P === "" || R === "" || N === "") {
+        alert("Please enter all values");
+        return;
+    }
+
+    let monthlyRate = R / 12 / 100;
+
+    let emi = (P * monthlyRate * Math.pow(1 + monthlyRate, N)) /
+              (Math.pow(1 + monthlyRate, N) - 1);
+
+    let totalPayment = emi * N;
+    let totalInterest = totalPayment - P;
+
+    document.getElementById("emi").innerText = "₹ " + emi.toFixed(2);
+    document.getElementById("totalInterest").innerText = "₹ " + totalInterest.toFixed(2);
+    document.getElementById("totalPayment").innerText = "₹ " + totalPayment.toFixed(2);
+}
